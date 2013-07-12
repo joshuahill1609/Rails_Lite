683a60c6c93f0c02538481faf018820d6220c875
App Academy Student <student@appacademy.io>
2013-07-12 18:07:44 -0400

    building phase vc

--- lib/rails_lite/params.rb
+++ lib/rails_lite/params.rb
@@ -1,40 +1,51 @@
 require 'uri'
 
 class Params
-  def initialize(req)
-    # @params = {}
-    #add route_params back
-    query_string = req.query_string
-    @query = parse_www_encoded_form(query_string)
+  def initialize(req, route_params=nil)
+    @params = {}
+    if req.body
+      @params.merge!(parse_www_encoded_form(req.body))
+    end
+    if req.query_string
+      query_string = req.query_string
+      query = parse_www_encoded_form(query_string)
+      @params.merge!(query)
+    end
   end
 
   def [](key)
   end
 
   def to_s
-    @query.to_json.to_s
-
+    @params.to_json.to_s
   end
 
   private
   def parse_www_encoded_form(www_encoded_form)
-    decoded_values = URI.decode_www_form(www_encoded_form)
-    decoded_values
+    unless www_encoded_form.nil?
+      hash = 'hahahaha'
+      decoded_values = URI.decode_www_form(www_encoded_form)
+      x = decoded_values.
+      p x.first
+      p x[1]
+      p x.first.scan(\W?(\w+)\W?)
+      # y = x.first.scan(/\w+/)
+#       p y + x.last.scan(/\w+/)
+#       no = y + x.last.scan(/\w+/)
+#       a_hash = {no.first => {no.second => x[1]}}
+#       p a_hash
+      p "!!!!!!!!!"
+      hash = Hash.new
+      decoded_values.each do |keypair|
+        hash[keypair.first] = keypair.last
+      end
 
-    hash = Hash.new
-    decoded_values.each do |keypair|
-      hash[keypair.first] = keypair.last
+      #\W?(\w+)\W?
+      hash
     end
-    hash
-
-  #   value = decoded_values.first
-  #   second = decoded_values.last
-  #   hash = Hash.new
-  #   hash[first.first] = first.last
-  #   hash[second.first.to_sym] = second.last
-  #   hash
   end
 
   def parse_key(key)
+
   end
 end
